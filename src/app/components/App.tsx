b371dba74623bfc0cf682830c42c9c5fadacfc3f
Gary <gary@codesbiome.com>
2020-05-25 22:07:01 +0530

    Converted App component to Functional

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,31 +1,18 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-import HomeScreen from '../screens/HomeScreen';
+import { withRouter } from 'react-router-dom';
 import Header from './Header';
 import CounterWidget from './CounterWidget';
 import '../styles/index.less';
 
-interface AppProps extends RouteComponentProps {
-  title?: string;
-}
-
-interface AppState {
-  counter: number;
-}
-
-class App extends React.Component<AppProps, AppState> {
-  readonly state: AppState = { counter: 0 };
-
-  render(): React.ReactElement {
-    return (
-      <div className='container'>
-        <Header />
-        <CounterWidget />
-      </div>
-    );
-  }
-}
+const App: React.FC = () => {
+  return (
+    <div className='container'>
+      <Header />
+      <CounterWidget />
+    </div>
+  );
+};
 
 const AppWithRouter = withRouter(App);
 
