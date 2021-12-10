2cca0d08dce7f3fe400f4630ad71eafeb70ee2db
Mamadou Wann <mamadouwann@gmail.com>
2021-12-09 22:48:25 -0500

    setting up firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,2 @@
+import firebase from "firebase";
+import config from "tools/firebaseConfig";
---
+++ tools/firebaseConfig.ts
@@ -0,0 +1,12 @@
+
+const config = {
+apiKey: process.env.REACT_APP_API_KEY,
+authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+databaseURL: process.env.REACT_APP_DB,
+projectId: process.env.REACT_APP_PID,
+storageBucket: process.env.REACT_APP_SB,
+messagingSenderId: process.env.REACT_APP_SID,
+appId: process.env.REACT_APP_APPID,
+measurementId:process.env.REACT_APP_MID
+};
+export default config;
